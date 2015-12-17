821d51c772ff57bf23196345ae617ee3673e211c
62mkv <62mkv@mail.ru>
2015-12-17 17:01:47 +0600

    added pydal-based working example

---
+++ redmine_pydal.py
@@ -0,0 +1,24 @@
+﻿from sshtunnel import SSHTunnelForwarder
+import settings
+from pydal import DAL, Field
+
+def dal_connect():
+    server = SSHTunnelForwarder(
+             (settings.ssh_host, settings.ssh_port),
+             ssh_password=settings.ssh_password,
+             ssh_username=settings.ssh_username,
+             remote_bind_address=('127.0.0.1', 3306))
+
+    server.start()
+
+    uri = 'mysql://{username}:{password}@127.0.0.1:{port}/{db}'.format(
+           username = settings.mysql_username,
+           password = settings.mysql_password,
+           db = settings.mysql_dbname,
+           port = server.local_bind_port
+        )
+
+    print "Creating db"
+    db = DAL(uri, migrate = False)
+    print "Created db"
+    return db
---
+++ test-pydal.py
@@ -0,0 +1,28 @@
+﻿import settings
+from redmine_pydal import dal_connect
+from pydal import Field
+
+db = dal_connect()
+db.define_table('users',Field('firstname'),Field('lastname'))
+db.users.name = Field.Virtual('name', lambda r:r['firstname'][0]+' '+r['lastname'])
+db.define_table('statuses',Field('name'), rname='issue_statuses')
+db.define_table('issues',
+     Field('subject'),
+     Field('parent_id'),
+     Field('fixed_version_id'),
+     Field('status_id', 'reference statuses'),
+     Field('estimated_hours','float'),
+     Field('assigned_to_id','reference users')
+    )
+in_current_milestone = db.issues.fixed_version_id == settings.current_milestone
+has_parent = ~(db.issues.parent_id == None)
+closed = db.issues.status_id.belongs(5,7)
+no_estimate = (db.issues.estimated_hours == None) | (db.issues.estimated_hours < .01)
+not_done = db.issues.status_id.belongs(settings.statuses_not_done)
+
+query = ~closed & in_current_milestone & has_parent & no_estimate & not_done
+
+print db(query)._select()
+rows = db(query).select()
+for row in rows:
+    print row.id, row.assigned_to_id.lastname, row.subject, row.status_id.name
