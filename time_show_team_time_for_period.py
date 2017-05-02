06d5e54dab472d4fecc76b8ef5b18f2b31f4a264
Kirill Marchuk <62mkv@mail.ru>
2017-05-02 11:18:21 +0700

    added options to specify discrete start/end of analyzed period

--- time_show_team_time_for_period.py
+++ time_show_team_time_for_period.py
@@ -1,14 +1,29 @@
 ﻿from time_entry import print_team_time_spent_on_period
-from datetime import date
+from datetime import date, datetime
 from rminstance.common import teams
-import sys
+
+def valid_date(string):
+    try:
+        dt = datetime.strptime(string,"%d-%m-%Y")
+    except:
+        msg = "%r is not a valid date" % string
+        raise argparse.ArgumentTypeError(msg)
+    return date(dt.year,dt.month,dt.day)
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--team", help='one or several (comma-separated) of keys of "teams" dict, declared in rminstance/common.py. When a valid team is specified, only data for this team is collected.')
+parser.add_argument("--from", dest='fromdate', help='specify start date of period', type=valid_date)
+parser.add_argument("--to", dest='todate', help='specify end date of period', type=valid_date)
+args=parser.parse_args()
 
 try:
-    arg_teams=sys.argv[1]
+    arg_teams=args.team
     teamdict = {team:teams[team] for team in (set(teams.keys()) & set(arg_teams.split(',')))}
 except:
     teamdict = teams
 
 today = date.today()
-start_of_month = today.replace(day=1)
-print_team_time_spent_on_period(teamdict, (start_of_month, today))
+start_of_period = args.fromdate if args.fromdate else today.replace(day=1)
+end_of_period = args.todate if args.todate else today
+print_team_time_spent_on_period(teamdict, (start_of_period, end_of_period))
